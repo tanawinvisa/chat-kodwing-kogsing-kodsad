f58405c596054e82157602be834b16363409db72
tanawinvisa <tanawinvisa@gmail.com>
2024-04-23 22:45:25 +0700

    fix padding list group

--- client/src/pages/Left-Sidebar/list-group.tsx
+++ client/src/pages/Left-Sidebar/list-group.tsx
@@ -101,7 +101,7 @@ const Groups: React.FC<ChatGroupsProps> = ({
   ];
 
   return (
-    <div className="bg-white w-1/3 border-r dark:bg-gray-800 border-gray-300 py-2 h-full flex flex-col">
+    <div className="bg-white w-1/3 border-r dark:bg-gray-800 border-gray-300 h-full flex flex-col">
       <div className="h-20 w-full border-b border-gray-300 dark:border-white flex items-center justify-center space-x-4">
         <form
           className="w-2/3 flex items-center relative"
